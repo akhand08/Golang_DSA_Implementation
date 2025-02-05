60395460ae09e831f3fecc751d1af9396eb3226e
akhand08 <adib08@outlook.com>
2025-02-05 19:11:17 +0600

    Add element by index functionality has been added

--- singlyLinkedList/linkedlist.go
+++ singlyLinkedList/linkedlist.go
@@ -1,22 +1,23 @@
 package singlylinkedlist
 
+import "fmt"
+
 type Node struct {
 	Data int
 	next *Node
 }
 
-type SingleLinkedList struct {
+type SinglyLinkedList struct {
 	head *Node
 }
 
 // function to create a new Singly Linked List
-func New() *SingleLinkedList {
-	return &SingleLinkedList{}
+func New() *SinglyLinkedList {
+	return &SinglyLinkedList{}
 }
 
 // Insert new element at the end of the Singly Linked List
-
-func (list *SingleLinkedList) Append(data int) {
+func (list *SinglyLinkedList) Append(data int) {
 	newNode := &Node{Data: data, next: nil}
 
 	if list.head == nil {
@@ -35,8 +36,7 @@ func (list *SingleLinkedList) Append(data int) {
 }
 
 // Insert new element at the beginning of the Singly Linked List
-
-func (list *SingleLinkedList) Prepend(data int) {
+func (list *SinglyLinkedList) Prepend(data int) {
 	newNode := &Node{Data: data, next: nil}
 
 	if list.head == nil {
@@ -46,4 +46,39 @@ func (list *SingleLinkedList) Prepend(data int) {
 
 	newNode.next = list.head
 	list.head = newNode
+}
+
+// Add new element on the given index
+func (list *SinglyLinkedList) AddByIndex(data int, index int) error {
+
+	if index < 0 {
+		return fmt.Errorf("Index out of Range")
+	}
+
+	counter := 0
+	currentNode := list.head
+
+	for counter < index && currentNode != nil {
+		currentNode = currentNode.next
+		counter += 1
+	}
+
+	newNode := &Node{Data: data, next: nil}
+
+	if currentNode == nil && index == 0 {
+		list.head = newNode
+	} else if currentNode == nil && counter < index-1 {
+		return fmt.Errorf("Index out of Range")
+	} else if index == 0 {
+
+		newNext := list.head
+		list.head = newNode
+		newNode.next = newNext
+
+	} else {
+		oldNext := currentNode.next
+		currentNode.next = newNode
+		newNode.next = oldNext
+	}
+
 }
